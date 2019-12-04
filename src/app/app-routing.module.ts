ea4f96e4c277fd756ea238458e5e660c7f1085a0
JulianEcheverri <julianecheverri@outlook.com>
2019-12-04 13:24:04 -0500

    Creating the app"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { HeroeComponent } from "./components/heroe/heroe.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "heroes", component: HeroesComponent },
+  { path: "heroe/:id", component: HeroeComponent },
+  { path: "**",  pathMatch: 'full',  redirectTo: 'heroes' }
+];
 
 @NgModule({
+  // importamos en el modulo la variable de rutas configuradas previamente
   imports: [RouterModule.forRoot(routes)],
+  // debemos exportar el modulo para que pueda ser usado de manera global
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+// !!se debe declarar en los imports del app.module.ts
