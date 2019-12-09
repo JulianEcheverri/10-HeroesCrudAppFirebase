9b7c2164b07439d098c52d2c3060a15b58542707
JulianEcheverri <julianecheverri@outlook.com>
2019-12-09 13:31:20 -0500

    Update heroe firebase

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, NgForm } from "@angular/forms";
 import { HeroeModel } from "../../models/heroe.model";
-import { HeroesService } from '../../services/heroes.service';
+import { HeroesService } from "../../services/heroes.service";
+import Swal from "sweetalert2";
+import { Observable } from "rxjs";
+
 @Component({
   selector: "app-heroe",
   templateUrl: "./heroe.component.html",
@@ -12,20 +15,41 @@ export class HeroeComponent implements OnInit {
   formulario: FormGroup;
   heroe = new HeroeModel();
 
-  constructor(private heroesService: HeroesService) {
-  }
+  constructor(private heroesService: HeroesService) {}
 
   guardar(form: NgForm) {
     if (form.invalid) {
-      console.log('Formulario no valido');
+      console.log("Formulario no valido");
       return;
     }
     // console.log(form);
-     console.log(this.heroe);
-     this.heroesService.crearHeroe(this.heroe).subscribe(resp=> {
-        console.log(resp);
-     });
-  }
+    console.log(this.heroe);
 
+    // para cuando se guarde dispare el alert
+    Swal.fire({
+      title: "Espere",
+      text: "Guardando información",
+      icon: "info",
+      allowOutsideClick: false
+    });
+
+    Swal.showLoading();
+
+    // variable observable para obtener los resultados de los dos observables del servicio
+    let peticion: Observable<any>;
+
+    // si existe id, lo actualiza, sino lo crea
+    peticion = this.heroe.id
+      ? this.heroesService.actualizarHeroe(this.heroe)
+      : this.heroesService.crearHeroe(this.heroe);
+
+    peticion.subscribe(resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: "Se actualizó correctamente",
+        icon: "success",
+      });
+    });
+  }
   ngOnInit() {}
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,33 +1,41 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HeroeModel } from '../models/heroe.model';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HeroeModel } from "../models/heroe.model";
 // el operador map sirve para transformar lo que un observable puede regresar
-import {map} from 'rxjs/operators';
-
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HeroesService {
-  private url = 'https://heroesapp-67019.firebaseio.com/'
+  private url = "https://heroesapp-67019.firebaseio.com/";
 
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { 
-
-  }
-
-  crearHeroe(heroe: HeroeModel){
+  crearHeroe(heroe: HeroeModel) {
     // .json para decirle a firebase que use su REST API
     // /8/ segundo argumento es el body, lo que se envia en la api
-    
-    // se pasa por el metodo pipe de los observables, dentro del pipe ya pyuedo usar los operadores 
-    return this.http.post(`${this.url}/heroes.json`, heroe)
-    .pipe(
+
+    // se pasa por el metodo pipe de los observables, dentro del pipe ya pyuedo usar los operadores
+    return this.http.post(`${this.url}/heroes.json`, heroe).pipe(
       map((resp: any) => {
         // en vez de devolver solo la respuesta de firebase, se devuelve el heroe con el id modificado
         heroe.id = resp.name;
         return heroe;
       })
     );
+  }
+
+  actualizarHeroe(heroe: HeroeModel) {
+    // creamos una variable que almacene el heroe temporal enviado y asi poder enviarle el objeto heroe sin id
+    const heroeTemp = {
+      ...heroe
+    };
+    // firebase al actualizar un registro, los datos que no encuentre en la bd, seran agredados (el id), 
+    // pero el id ya esta especificado como clave del objeto y por ende se borra de heroe
+    delete heroeTemp.id;
+
+    // put para actualizar el heroe
+    return this.http.put(`${this.url}/heroes/${heroe.id}.json`, heroeTemp);
   }
 }
